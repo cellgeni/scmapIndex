4148019e01dfd0d02542797d97e6bfaf02ca2064
wikiselev <vk6@sanger.ac.uk>
2018-04-23 13:39:14 +0100

    update R scripts

--- bin/scmap_cell.R
+++ bin/scmap_cell.R
@@ -1,17 +1,29 @@
 #!/usr/bin/env Rscript
 
 library(scmap)
+library(scater)
 library(SingleCellExperiment)
 
 args <- commandArgs(trailingOnly = TRUE)
 
 sce <- readRDS(args[1])
-rowData(sce)$feature_symbol <- rownames(counts(sce))
 
-sce <- selectFeatures(sce, suppress_plot = FALSE)
+# this function writes to logcounts slot
+exprs(sce) <- log2(calculateCPM(sce, use.size.factors = FALSE) + 1)
+# use gene names as feature symbols
+rowData(sce)$feature_symbol <- rownames(sce)
+# remove features with duplicated names
+if(is.null(rowData)) {
+    sce <- sce[!duplicated(rowData(sce)$feature_symbol), ]
+}
+# QC
+isSpike(sce, "ERCC") <- grepl("^ERCC-", rownames(sce))
+
+# scmap requires cell_type1 column for the reference
+colnames(colData(sce))[grepl("ClusterID", colnames(colData(sce)))] <- "cell_type1"
 
 sce <- indexCell(sce)
 saveRDS(
     metadata(sce)$scmap_cell_index,
-    file = paste(strsplit("\\.", args[1])[[1]][1], ".rds")
+    file = paste0(strsplit(args[1], "\\.")[[1]][1], ".rds")
 )
--- bin/scmap_cluster.R
+++ bin/scmap_cluster.R
@@ -1,18 +1,32 @@
 #!/usr/bin/env Rscript
 
 library(scmap)
+library(scater)
 library(SingleCellExperiment)
 
 args <- commandArgs(trailingOnly = TRUE)
 
 sce <- readRDS(args[1])
-rowData(sce)$feature_symbol <- rownames(counts(sce))
 
-sce <- selectFeatures(sce, suppress_plot = FALSE)
+# this function writes to logcounts slot
+exprs(sce) <- log2(calculateCPM(sce, use.size.factors = FALSE) + 1)
+# use gene names as feature symbols
+rowData(sce)$feature_symbol <- rownames(sce)
+# remove features with duplicated names
+if(is.null(rowData)) {
+    sce <- sce[!duplicated(rowData(sce)$feature_symbol), ]
+}
+# QC
+isSpike(sce, "ERCC") <- grepl("^ERCC-", rownames(sce))
 
+# scmap requires cell_type1 column for the reference
+colnames(colData(sce))[grepl("ClusterID", colnames(colData(sce)))] <- "cell_type1"
+
+# run scmap
+sce <- selectFeatures(sce)
 sce <- indexCluster(sce)
 write.csv(
     metadata(sce)$scmap_cluster_index, 
     quote = F,
-    file = paste(strsplit("\\.", args[1])[[1]][1], ".csv")
+    file = paste0(strsplit(args[1], "\\.")[[1]][1], ".csv")
 )
